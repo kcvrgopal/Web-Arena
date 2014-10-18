ea6603302d8ce5d510066ad6ee720649f400e084
RAJAGOPALAN VENKATARAMANAN <rajagopalan@RajagopalansMBP.T-mobile.com>
2014-10-18 02:35:00 -0700

    Added web socket server using node.js express

---
+++ Web Sockets Server example/app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+var server = http.createServer(function(request, response) {});
+var routes = require('./routes/index');
+var users = require('./routes/users');
+server.listen(1234, function() {
+    console.log((new Date()) + ' Server is listening on port 1234');
+});
+var WebSocketServer = require('websocket').server;
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+var wsServer = new WebSocketServer({
+    httpServer: server
+}).on('request', function(r){
+    // Code here to run on connection
+        var connection = r.accept('echo-protocol', r.origin);
+        connection.on('message', function(message)
+        {
+            console.log(message.utf8Data);
+            connection.sendUTF("Received scores : "+message.utf8Data+"<br> Exams Graded");
+        });
+
+
+    });
+module.exports = app;
